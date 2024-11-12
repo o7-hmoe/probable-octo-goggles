a5bbe5948a6addcd05749e39a5cc4a5a727cbbda
H.M <hunter.moe7@gmail.com>
2024-11-12 23:46:05 +0100

    Update discharge_analysis.py

--- discharge_analysis.py
+++ discharge_analysis.py
@@ -14,21 +14,27 @@ df = pd.read_csv("flow-data/daily-flow-series.csv",
                  index_col="Date")
 print(df.head())
 
+# Plot the daily flow data over time
 plot_discharge(df.index,df["Q (CMS)"], title="Daily Flow Data")
 
+# Resample data to get annual maximum flow values
 annual_max_df = df.resample(rule="YE", kind="period").max()
 annual_max_df["year"] = annual_max_df.index.year
 annual_max_df.reset_index(inplace=True, drop=True)
 print(annual_max_df.head())
 plot_discharge(annual_max_df["year"], annual_max_df["Q (CMS)"], title="Wasserburg a. Inn 1826 - 2016 (annual)")
 
+# Sort data by flow values
 annual_max_df_sorted = annual_max_df.sort_values(by="Q (CMS)")
 
+# Find total number of entries and assign a rank to each one
 n = annual_max_df_sorted.shape[0]
 annual_max_df_sorted.insert(0, "rank", range(1, 1 + n))
 
+# Calculate probability of exceedance
 annual_max_df_sorted["pr"] = (n - annual_max_df_sorted["rank"] + 1) / (n + 1)
 
+# Calculate return period
 annual_max_df_sorted["return-period"] = 1 / annual_max_df_sorted["pr"]
 
 print(annual_max_df_sorted.tail())
